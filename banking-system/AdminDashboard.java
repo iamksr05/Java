8d55297291a7d14e68f20f5383326fef3750c2c1
Karan Ram <Karanram73005@gmail.com>
2026-03-15 15:53:58 +0530

    added AdminDashboard

---
+++ banking-system/AdminDashboard.java
@@ -0,0 +1,15 @@
+public class AdminDashboard {
+    void adminOption() {
+
+        while (true) {
+            System.out.println("1. Create Account");
+            System.out.println("2. Delete Account");
+            System.out.println("3. Modify account");
+            System.out.println("1. Deposit Money");
+            System.out.println("1. Withdraw Money");
+            System.out.println("1. View All Accounts");
+            System.out.println("1. Logout");
+
+        }
+    }
+}
--- banking-system/Main.java
+++ banking-system/Main.java
@@ -1,5 +1,63 @@
-public class Main {
+import java.util.Scanner;
+
+class Main {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
+
+        System.out.println("Welcome to PW Bank.Login as\n1. Admin\n2. Customer");
+
+        int choice = input.nextInt();
+
+        while (true) {
+            
+            if (choice == 1) {
+
+                break;
+                
+            } else if (choice == 2) {
+                
+                break;
+
+            } else {
+                System.out.println("Invalid Choice. Enter correct choice: ");
+            }
+
+        }
+    }
+
+}
+
+class AdminLogin {
+
+    void adminPass(Scanner input) {
+        System.out.println("Enter your user ID: ");
+        int adminId = input.nextInt();
         
+        System.out.println("Enter your Password: ");
+        int adminPass = input.nextInt();
+
+        if (adminId != 987654321 && adminPass != 1234) {
+            System.out.println("Incorrect credentials. Try again!");
+        } else {
+
+        }
+    }
+}
+
+
+class CustomerLogin {
+
+    void adminPass(Scanner input) {
+        System.out.println("Enter your user ID: ");
+        int userId = input.nextInt();
+        
+        System.out.println("Enter your user ID: ");
+        int userPass = input.nextInt();
+
+        if (userId != 987654321 && userPass != 1234) {
+            System.out.println("Incorrect credentials. Try again!");
+        } else {
+
+        }
     }
 }
